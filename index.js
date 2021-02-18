32a644093dbb21144c1d782cf498e062808d0895
SANUP <sanupkumar121999@gmail.com>
2021-02-18 21:48:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+
+const body_parser =  require("body-parser");
+
+
+
+app.use(body_parser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.listen(3000,function(err){
+  if(!err)
+  {
+    console.log("server is running at port 3000");
+  }
+  else{
+    console.log("error");
+  }
+});
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/",function(req,res){
+  var name = req.body.user_name;
+  var passcode = req.body.password;
+  console.log(name + " has logged in with password " + passcode);
+  res.redirect("/");
+});
